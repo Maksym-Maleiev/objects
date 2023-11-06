a391bc1ea206cf79226b0fd4d33f792b800886a5
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-11-06 14:17:28 +0200

    Update obj.js

--- obj.js
+++ obj.js
@@ -1,6 +1,15 @@
+// Object
 var cat = {
   "legs": 3,
   "name": "Harmony",
   "color": "Tortoiseshell" /* черепаховий */
+};
+cat;
+
+// keys without double quotes
+var cat = {
+  legs: 3,
+  name: "Harmony",
+  color: "Tortoiseshell"
 };
 cat;
